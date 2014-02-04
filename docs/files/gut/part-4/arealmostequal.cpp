d8f87e493513d05a5323134b5cae13dfe5cd773a
gianfranco <gianfranco.zuliani@gmail.com>
2014-02-04 11:52:49 +0100

    gut: some minor fixes

--- docs/files/gut/part-4/arealmostequal.cpp
+++ docs/files/gut/part-4/arealmostequal.cpp
@@ -5,7 +5,7 @@ const double pi = 3.141592653;
 
 gut::Boolean AreAlmostEqual(double d1, double d2) {
 	double diff = std::fabs(d1 - d2);
-	return gut::Boolean(diff < 0.001, gut::TextFlow() << "|" << d1 << " - " << d2 << "| = " << diff << " < 0.001");
+	return gut::Boolean(diff < 0.001, gut::TextFlow() << "|" << d1 << " - " << d2 << "| = " << diff << " >= 0.001");
 }
 
 TEST {
