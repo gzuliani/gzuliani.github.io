369dc93e6d7ee72b70662a488d3fdd41385956f5
gianfranco <gianfranco.zuliani@gmail.com>
2015-06-05 11:43:23 +0200

    gut: fixed DefaultReport, some refactoring

--- cpp/files/gut/part-14/gut.h
+++ cpp/files/gut/part-14/gut.h
@@ -167,13 +167,13 @@ class HasOperatorString {
     static yes& check_(...);
 
 public:
-    static const bool response = sizeof(yes) == sizeof(check_<Derived>(0));
+    static const bool value = sizeof(yes) == sizeof(check_<Derived>(0));
 };
 
 template<>
 class HasOperatorString<bool> {
 public:
-    static const bool response = false;
+    static const bool value = false;
 };
 
 template<typename T, bool HasOperatorString = true>
@@ -218,8 +218,8 @@ public:
         return static_cast<bool>(value_);
     }
     virtual std::string toString() const {
-        if (HasOperatorString<T>::response)
-            return StringRepr<T, HasOperatorString<T>::response>(value_).str();
+        if (HasOperatorString<T>::value)
+            return StringRepr<T, HasOperatorString<T>::value>(value_).str();
         else
             return gut::toString(static_cast<bool>(value_));
     }
