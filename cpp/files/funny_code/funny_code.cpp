b29926e4c579f7fb29a3ab674520a32d92760158
gianfranco <gianfranco.zuliani@gmail.com>
2017-02-03 17:57:27 +0100

    funny-code: initial draft

---
+++ cpp/files/funny_code/funny_code.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+const auto wrap = [](auto x) { return [x](auto) { return x; }; };
+const auto delay = [](auto x) { return x; };
+
+const auto first = wrap;
+const auto rest = wrap(delay);
+
+const auto pair = [](auto x) {
+  return [x](auto y) {
+    return [x, y](auto sel) {
+      return sel(x)(y);
+    };
+  };
+};
+
+const auto empty_list = [](auto when_empty, auto) { return when_empty(); };
+const auto node = [](auto x) {
+  return [x](auto y) {
+    return [x, y](auto, auto unless_empty) {
+      return unless_empty(pair(x)(y));
+    };
+  };
+};
+
+const auto one_item_list = node(1)(empty_list);
+const auto many_items_list = node(1)(node(2)(node(3)(empty_list)));
+
+template <class T>
+int length(T list, int acc) {
+  return list(
+    [acc]() { return acc; },
+    [acc](auto p) { return length(p(rest), acc+1); });
+}
+
+template <class T>
+int length(T list) {
+  return length(list, 0);
+}
+
+int main() {
+  std::cout
+    << length(empty_list) << " "
+    << length(one_item_list) << " "
+    << length(many_items_list) << "\n";
+}
